faac57fd0ec8d9657dc036107171699759707751
Filippe Braga <filippeffx@hotmail.com>
2023-03-31 21:36:57 -0300

    initial commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
 export default function About() {
   return (
-    <h1 className="text-3xl flex justify-center items-center border-2 border-slate-600">
-      Hello World!
-    </h1>
+    <h1 className="text-3xl flex justify-center items-center">Hello World!</h1>
   );
 }
